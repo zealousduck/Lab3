61fd54213d98b0f8914804b45aa3e1c89f06810c
Olivia Murphy <onm0002@auburn.edu>
2015-11-20 17:48:58 -0600

    pushing server again

--- udp_server.c
+++ udp_server.c
@@ -64,95 +64,106 @@ int main(int argc, char *argv[])
             fprintf(stderr, "listener: failed to bind socket\n");
             return 2;
         }
-    
+
 
 	freeaddrinfo(servinfo);
 
 	printf("listener: waiting to recvfrom...\n");
 
-	addr_len = sizeof their_addr;
-	if ((numbytes = recvfrom(sockfd, buf, 5, 0,
-		(struct sockaddr *)&their_addr, &addr_len)) == -1) {
-		perror("recvfrom");
-		exit(1);
-	}
-
-	int ipAddr = (int)(their_addr.sin_addr.s_addr);
-	uint16_t portAddr = ntohs(their_addr.sin_port);
-
-    int theirGID = buf[4];
-    int theirPort = (buf[2] << 8) + buf[3];
-
-	//check for errors
-    uint8_t errorCode = 0x00;
-    
-    if (buf[0] != magicNumber && buf[1] != magicNumber) {
-        errorCode += 1;
-    }
-    
-    if (numbytes != 5) {
-        errorCode += 2;
-    }
-    
-    if (theirPort < (10010 + 5 * theirGID) || theirPort > (10010 + 5 * theirGID + 4)) {
-        errorCode += 4;
-    }
-    
-    //if error, send error packet
-	if (errorCode != 0x00) {
-		char errorPacket[5];
-		memset(errorPacket,0,5);
-		errorPacket[0] = (unsigned char)0xA5;
-		errorPacket[1] = (unsigned char)0xA5;
-		errorPacket[2] = (unsigned char)1;
-        errorPacket[3] = (unsigned char)0x00;
-        errorPacket[4] = (unsigned char)errorCode;
-
-		if ((numbytes = sendto(sockfd, (char*)&errorPacket, 5, 0,
-			p->ai_addr, p->ai_addrlen)) == -1) {
-			perror("sendto");
-			exit(1);
-		}
-	}
-
-	if (waiting == 0) {
-		waiting = 1;
-		char noWaitPacket[5];
-		memset(noWaitPacket,0,5);
-		noWaitPacket[0] = (unsigned char)0xA5;
-		noWaitPacket[1] = (unsigned char)0xA5;
-		noWaitPacket[2] = (unsigned char)1;
-		noWaitPacket[3] = (unsigned char)portAddr >> 8;
-		noWaitPacket[4] = (unsigned char)portAddr >> 0;
-
-		if ((numbytes = sendto(sockfd, (char*)&noWaitPacket, 5, 0,
-			p->ai_addr, p->ai_addrlen)) == -1) {
-			perror("sendto");
+	while (1) {
+		addr_len = sizeof their_addr;
+		if ((numbytes = recvfrom(sockfd, buf, 5, 0,
+			(struct sockaddr *)&their_addr, &addr_len)) == -1) {
+			perror("recvfrom");
 			exit(1);
 		}
 
-		waiting = 0;
-	}
-
-	if (waiting == 1) {
-		char waitingPacket[9];
-		memset(waitingPacket,0,9);
-		waitingPacket[0] = (unsigned char)0xA5;
-		waitingPacket[1] = (unsigned char)0xA5;
-		waitingPacket[2] = (unsigned char)ipAddr >> 24;
-        waitingPacket[3] = (unsigned char)ipAddr >> 16;
-        waitingPacket[4] = (unsigned char)ipAddr >> 8;
-        waitingPacket[5] = (unsigned char)ipAddr >> 0;
-		waitingPacket[6] = (unsigned char)portAddr >> 8;
-		waitingPacket[7] = (unsigned char)portAddr >> 0;
-		waitingPacket[8] = (unsigned char)1;
-
-		if ((numbytes = sendto(sockfd, (char*)&waitingPacket, 9, 0,
-			p->ai_addr, p->ai_addrlen)) == -1) {
-			perror("sendto");
-			exit(1);
+		for (int i = 0; i <= strlen(buf); i++) {
+			printf("0x%x", buf[i] & 0xff);
+			printf("\n");
 		}
-	}
+
+		int ipAddr = (int)(their_addr.sin_addr.s_addr);
+		uint16_t portAddr = ntohs(their_addr.sin_port);
+
+    	int theirGID = buf[4];
+    	int theirPort = (buf[2] << 8) + buf[3];
+	
+		int magic = 0xa5;
+   
+   		uint8_t errorCode= 0x00;
+   		
+    	if ((buf[0] & 0xff) != magic && (buf[1] & 0xff) != magic) {
+    	   errorCode += 1;
+    	}
+
+    	printf("%u", errorCode);
+    	if (numbytes != 5) {
+    	    errorCode += 2;
+    	}
+    	printf("%u", errorCode);
+
+    	if (theirPort < (10010 + 5 * theirGID) || theirPort > (10010 + 5 * theirGID + 4)) {
+    	    errorCode += 4;
+    	}
+    	printf("%u", errorCode);
+    	//if error, send error packet
+		if (errorCode != 0x00) {
+			char errorPacket[5];
+			memset(errorPacket,0,5);
+			errorPacket[0] = (unsigned char)0xA5;
+			errorPacket[1] = (unsigned char)0xA5;
+			errorPacket[2] = (unsigned char)1;
+        	errorPacket[3] = (unsigned char)0x00;
+        	errorPacket[4] = (unsigned char)errorCode;
+
+			if ((numbytes = sendto(sockfd, (char*)&errorPacket, 5, 0,
+				(struct sockaddr *)&their_addr, addr_len)) == -1) {
+				perror("sendto");
+				exit(1);
+			}
+		}
+
+		if (waiting == 0) {
+			waiting = 1;
+			char noWaitPacket[5];
+			memset(noWaitPacket,0,5);
+			noWaitPacket[0] = (unsigned char)0xA5;
+			noWaitPacket[1] = (unsigned char)0xA5;
+			noWaitPacket[2] = (unsigned char)1;
+			noWaitPacket[3] = (unsigned char)portAddr >> 8;
+			noWaitPacket[4] = (unsigned char)portAddr >> 0;
+
+			if ((numbytes = sendto(sockfd, (char*)&noWaitPacket, 5, 0,
+				(struct sockaddr *)&their_addr, addr_len)) == -1) {
+				perror("sendto");
+				exit(1);
+			}
+
+			waiting = 0;
+		}
+
+		if (waiting == 1) {
+			char waitingPacket[9];
+			memset(waitingPacket,0,9);
+			waitingPacket[0] = (unsigned char)0xA5;
+			waitingPacket[1] = (unsigned char)0xA5;
+			waitingPacket[2] = (unsigned char)ipAddr >> 24;
+        	waitingPacket[3] = (unsigned char)ipAddr >> 16;
+        	waitingPacket[4] = (unsigned char)ipAddr >> 8;
+        	waitingPacket[5] = (unsigned char)ipAddr >> 0;
+			waitingPacket[6] = (unsigned char)portAddr >> 8;
+			waitingPacket[7] = (unsigned char)portAddr >> 0;
+			waitingPacket[8] = (unsigned char)1;
+
+			if ((numbytes = sendto(sockfd, (char*)&waitingPacket, 9, 0,
+				(struct sockaddr *)&their_addr, addr_len)) == -1) {
+				perror("sendto");
+				exit(1);
+			}
+
+		}
+	}	
 
 	close(sockfd);
 
